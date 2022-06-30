41620c2b4a0696b2c320a4963aa07103ee331b71
Lorraine <moyolorrain98@gmail.com>
2022-06-30 21:18:20 +0200

    Added old game files
    Co-authored-by: Beautus <sduduzog@users.noreply.github.com>

--- src/app.vue
+++ src/app.vue
@@ -1,32 +1,60 @@
-<script>
-import { ref } from "vue";
-export default {
-  setup() {
-    const score = ref(0);
-    const increment = () => {
-      score.value++;
-      console.log(score.value);
-    };
+<script setup>
+import { ref, computed } from "vue";
 
-    return { count: score, increment };
-  },
-};
+const newTodo = ref("");
+const hideCompleted = ref(false);
+const todos = ref([]);
+
+function addTodo() {
+  todos.value = [
+    ...todos.value,
+    {
+      id: todos.value.length + 1,
+      text: newTodo.value,
+      done: false,
+    },
+  ];
+  newTodo.value = "";
+}
+
+const filteredTodos = computed(() => {
+  // return filtered todos based on
+  // `todos.value` & `hideCompleted.value`
+  if (hideCompleted.value) {
+    return todos.value.filter((element) => {
+      if (element.done === false) {
+        return true;
+      }
+      return false;
+    });
+  }
+  return todos.value;
+});
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo);
+}
 </script>
+
 <template>
-  <div>
-    <h1 class="number">{{ count }}</h1>
-    <button class="btn" @click="increment">Increment</button>
-  </div>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? "Show all" : "Hide completed" }}
+  </button>
 </template>
 
 <style>
-div {
-  display: grid;
-  grid-gap: 100px;
-  justify-content: center;
-  padding: 20px;
-}
-.number {
-  text-align: center;
+.done {
+  text-decoration: line-through;
 }
 </style>
