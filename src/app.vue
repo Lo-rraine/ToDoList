0d643304dd00851672a8a6903827a03989aadf31
Lorrainem <moyolorraine98@gmail.com>
2022-07-12 22:05:26 +0200

    Landing screen with button

--- src/app.vue
+++ src/app.vue
@@ -1,60 +1,25 @@
 <script setup>
-import { ref, computed } from "vue";
-
-const newTodo = ref("");
-const hideCompleted = ref(false);
-const todos = ref([]);
-
-function addTodo() {
-  todos.value = [
-    ...todos.value,
-    {
-      id: todos.value.length + 1,
-      text: newTodo.value,
-      done: false,
-    },
-  ];
-  newTodo.value = "";
-}
-
-const filteredTodos = computed(() => {
-  // return filtered todos based on
-  // `todos.value` & `hideCompleted.value`
-  if (hideCompleted.value) {
-    return todos.value.filter((element) => {
-      if (element.done === false) {
-        return true;
-      }
-      return false;
-    });
-  }
-  return todos.value;
-});
-
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo);
+function startGame() {
+  alert("text");
 }
 </script>
 
 <template>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" />
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done" />
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? "Show all" : "Hide completed" }}
-  </button>
+  <div class="screen">
+    <button class="start-button" @click="startGame">Start Game</button>
+  </div>
 </template>
 
 <style>
-.done {
-  text-decoration: line-through;
+.screen {
+  background-color: aquamarine;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1000px;
+}
+
+.start-button {
+  padding: 10px;
 }
 </style>
