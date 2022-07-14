acee8d413d3bbdcaab7f0ef1bb9e60104fc85bf2
Lorrainem <moyolorraine98@gmail.com>
2022-07-14 22:06:26 +0200

    pinia store creation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import router from "./router";
-
+import { createPinia } from 'pinia'
 import App from "./app.vue";
 
 const app = createApp(App);
+const pinia = createPinia();
 app.use(router);
+app.use(pinia);
 app.mount("#app");
