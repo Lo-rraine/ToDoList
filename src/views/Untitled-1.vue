6e77c835262592eb6038d52c32ed2658e6d921bd
Lorrainem <moyolorraine98@gmail.com>
2022-07-14 22:06:09 +0200

    install and created store

---
+++ src/views/Untitled-1.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref, computed } from "vue";
+
+const newTodo = ref("");
+const hideCompleted = ref(false);
+const git branch
+ = ref([]);
+
+function addTodo() {
+  todos.value = [
+    ...todos.value,
+    {
+      id: todos.value.length + 1,
+      text: newTodo.value,
+      done: false,
+    },
+  ];
+  newTodo.value = "";
+}
+
+cgit onst filteredTodos = computed(() => {
+  // return filtered todos based on
+  // `todos.value` & `hideCompleted.value`
+  if (hideCompleted.value) {
+    return todos.value.filter((element) => {
+      if (element.done === false) {
+        return true;
+      }
+      return false;
+    });
+  }
+  return todos.value;
+});
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo);
+}
+</script>
+
+<template>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? "Show all" : "Hide completed" }}
+  </button>
+</template>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
